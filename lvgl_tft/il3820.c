1c2e6c30cc6ac7bbdb0e553520a3e62b945967fa
pguab <prtgb@outlook.com>
2022-04-02 10:16:23 +0200

    Fix IL3820 driver for Waveshare 2.9inch e-Paper Module
    Works for all 4 orientations, add color inverting.

--- lvgl_tft/il3820.c
+++ lvgl_tft/il3820.c
@@ -64,7 +64,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 
 #define IL3820_PIXELS_PER_BYTE		8
 
-uint8_t il3820_scan_mode = IL3820_DATA_ENTRY_XIYIY;
+uint8_t il3820_scan_mode = IL3820_DATA_ENTRY_XIYIX;
 
 static uint8_t il3820_lut_initial[] = {
     0x50, 0xAA, 0x55, 0xAA, 0x11, 0x00, 0x00, 0x00,
@@ -101,44 +101,76 @@ static void il3820_send_data(uint8_t *data, uint16_t length);
 static inline void il3820_set_window( uint16_t sx, uint16_t ex, uint16_t ys, uint16_t ye);
 static inline void il3820_set_cursor(uint16_t sx, uint16_t ys);
 static void il3820_update_display(void);
-static void il3820_clear_cntlr_mem(uint8_t ram_cmd, bool update);
+static void il3820_set_absolute_px(lv_disp_drv_t * disp_drv, uint8_t* buf,
+                                   lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+                                   lv_color_t color, lv_opa_t opa);
 
 /* Required by LVGL */
 void il3820_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
 {
-    /* Each byte holds the data of 8 pixels, linelen is the number of bytes
-     * we need to cover a line of the display. */
-    size_t linelen = EPD_PANEL_WIDTH / 8;
+    ESP_LOGD( TAG, "il3820_flush(x1=%d, y1=%d, x2=%d, y2=%d)",
+              area->x1, area->y1, area->x2, area->y2);
 
     uint8_t *buffer = (uint8_t*) color_map;
     uint16_t x_addr_counter = 0;
     uint16_t y_addr_counter = 0;
+    size_t area_width = 0;
+    size_t area_height = 0;
+    uint8_t entry_mode;
+    lv_area_t window;
 
-    /* Configure entry mode  */
-    il3820_write_cmd(IL3820_CMD_ENTRY_MODE, &il3820_scan_mode, 1);
+    /* Partial refresh is available but IL3820 use internally some weird double-buffering
+     * where not refreshed parts are updated by penultimate (not last) version of data.
+     * So always update whole screen. */
+#if defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE) || \
+    defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT)  || \
+    defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT_INVERTED)
+    entry_mode = IL3820_DATA_ENTRY_XIYIX;
 
-    /* Configure the window based on the coordinates got from LVGL
-     * It looks like this epaper display controller doesn't support partial update,
-     * so the window is always the same, the display size. */
-    il3820_set_window(0, EPD_PANEL_WIDTH - 1, 0, EPD_PANEL_HEIGHT - 1);
+    /* Each byte holds the data of 8 pixels */
+    area_width = EPD_PANEL_WIDTH >> 3;     /* in bytes */
+    area_height = EPD_PANEL_HEIGHT;
 
-    /* Set the cursor at the beginning of the graphic RAM */
-#if defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT)
+    window.x1 = 0;
+    window.x2 = EPD_PANEL_WIDTH - 1;
+    window.y1 = 0;
+    window.y2 = EPD_PANEL_HEIGHT - 1;
+
+    ESP_LOGV( TAG, "window(xs=%d, xe=%d, ys=%d, ye=%d), start(x=%d, y=%d), width=%d, height=%d",
+              window.x1, window.x2, window.y1, window.y2,
+              x_addr_counter, y_addr_counter, area_width, area_height);
+#elif defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED)
+    entry_mode = IL3820_DATA_ENTRY_XDYDX;
+
     x_addr_counter = EPD_PANEL_WIDTH - 1;
     y_addr_counter = EPD_PANEL_HEIGHT - 1;
+
+    /* Each byte holds the data of 8 pixels */
+    area_width = EPD_PANEL_WIDTH >> 3;     /* in bytes */
+    area_height = EPD_PANEL_HEIGHT;
+
+    window.x1 = EPD_PANEL_WIDTH - 1;
+    window.x2 = 0;
+    window.y1 = EPD_PANEL_HEIGHT - 1;
+    window.y2 = 0;
+
+    ESP_LOGV( TAG, "window(xs=%d, xe=%d, ys=%d, ye=%d), start(x=%d, y=%d)",
+              window.x1, window.x2, window.y1, window.y2, x_addr_counter, y_addr_counter);
+#else
+#error "Unsupported orientation used"
 #endif
+
+    il3820_write_cmd(IL3820_CMD_ENTRY_MODE, &entry_mode, 1);
+
+    il3820_set_window(window.x1, window.x2, window.y1, window.y2);
 
     il3820_set_cursor(x_addr_counter, y_addr_counter);
 
     il3820_send_cmd(IL3820_CMD_WRITE_RAM);
 
-    /* Write the pixel data to graphic RAM, linelen bytes at the time. */
-    for(size_t row = 0; row <= (EPD_PANEL_HEIGHT - 1); row++){
-	il3820_send_data(buffer, linelen);
-	buffer += IL3820_COLUMNS;
-    }
+    il3820_send_data(buffer, area_width * area_height);
 
-    il3820_set_window(0, EPD_PANEL_WIDTH - 1, 0, EPD_PANEL_HEIGHT - 1);
+    //il3820_set_window(0, EPD_PANEL_WIDTH - 1, 0, EPD_PANEL_HEIGHT - 1);
 
     il3820_update_display();
 
@@ -157,37 +189,68 @@ void il3820_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t* buf,
     lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color, lv_opa_t opa)
 {
-    uint16_t byte_index = 0;
-    uint8_t  bit_index = 0;
+    /* Pixel coordinates are relative to current area. As we always send whole buffer,
+     * we need to transform them to absolute. */
+    lv_disp_buf_t *vdb = disp_drv->buffer;
+    x += vdb->area.x1;
+    y += vdb->area.y1;
 
-#if defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT)
-    byte_index = x + ((y >> 3) * EPD_PANEL_HEIGHT);
-    bit_index  = y & 0x7;
-
-    if (color.full) {
-        BIT_SET(buf[byte_index], 7 - bit_index);
-    } else {
-        uint16_t mirrored_idx = (EPD_PANEL_HEIGHT - x) + ((y >> 3) * EPD_PANEL_HEIGHT);
-        BIT_CLEAR(buf[mirrored_idx], 7 - bit_index);
-    }
-#elif defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE)
-    byte_index = y + ((x >> 3) * EPD_PANEL_HEIGHT);
-    bit_index  = x & 0x7;
-
-    if (color.full) {
-        BIT_SET(buf[byte_index], 7 - bit_index);
-    } else {
-        BIT_CLEAR(buf[byte_index], 7 - bit_index);
-    }
+#if defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE) || defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED)
+    /* inversion is made in flush() */
+    il3820_set_absolute_px(disp_drv, buf, buf_w, x, y, color, opa);
+#elif defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT)
+    /* Left bottom corner has coordinates [0,0] */
+    lv_coord_t point_temp = x;
+    x = y;
+    y = EPD_PANEL_HEIGHT - point_temp - 1;
+    il3820_set_absolute_px(disp_drv, buf, buf_w, x, y, color, opa);
+#elif defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT_INVERTED)
+    /* Right top corner has coordinates [0,0] */
+    lv_coord_t point_temp = x;
+    x = EPD_PANEL_WIDTH - y - 1;
+    y = point_temp;
+    il3820_set_absolute_px(disp_drv, buf, buf_w, x, y, color, opa);
 #else
 #error "Unsupported orientation used"
 #endif
 }
 
+void il3820_set_absolute_px(lv_disp_drv_t * disp_drv, uint8_t* buf,
+    lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+    lv_color_t color, lv_opa_t opa)
+{
+    if (x < 0 || x >= EPD_PANEL_WIDTH || y < 0 || y >= EPD_PANEL_HEIGHT) {
+        return;
+    }
+    uint16_t byte_index = (x + y * EPD_PANEL_WIDTH) >> 3;
+    uint8_t  bit_index = x & 0x7;
+
+#if defined(CONFIG_LV_INVERT_COLORS)
+    if (color.full) {
+        BIT_CLEAR(buf[byte_index], 7 - bit_index);
+    } else {
+        BIT_SET(buf[byte_index], 7 - bit_index);
+    }
+#else
+    if (color.full) {
+        BIT_SET(buf[byte_index], 7 - bit_index);
+    } else {
+        BIT_CLEAR(buf[byte_index], 7 - bit_index);
+    }
+#endif
+}
+
 /* Required by LVGL */
 void il3820_rounder(lv_disp_drv_t * disp_drv, lv_area_t *area) {
+#if defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE) || defined (CONFIG_LV_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED)
     area->x1 = area->x1 & ~(0x7);
     area->x2 = area->x2 |  (0x7);
+#elif defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT) || defined (CONFIG_LV_DISPLAY_ORIENTATION_PORTRAIT_INVERTED)
+    area->y1 = area->y1 & ~(0x7);
+    area->y2 = area->y2 |  (0x7);
+#else
+#error "Unsupported orientation used"
+#endif
 }
 
 /* main initialization routine */
@@ -378,9 +441,9 @@ static void il3820_update_display(void)
     uint8_t tmp = 0;
 
     if(il3820_partial) {
-	tmp = IL3820_CTRL2_TO_PATTERN;
+        tmp = IL3820_CTRL2_TO_PATTERN;
     } else {
-	tmp =  (IL3820_CTRL2_ENABLE_CLK | IL3820_CTRL2_ENABLE_ANALOG | IL3820_CTRL2_TO_PATTERN);
+        tmp =  (IL3820_CTRL2_ENABLE_CLK | IL3820_CTRL2_ENABLE_ANALOG | IL3820_CTRL2_TO_PATTERN);
     }
 
     il3820_write_cmd(IL3820_CMD_UPDATE_CTRL2, &tmp, 1);
@@ -388,8 +451,8 @@ static void il3820_update_display(void)
     il3820_write_cmd(IL3820_CMD_MASTER_ACTIVATION, NULL, 0);
     /* Poll BUSY signal. */
     il3820_waitbusy(IL3820_WAIT);
-    /* XXX: Figure out what does this command do. */
-    il3820_write_cmd(IL3820_CMD_TERMINATE_FRAME_RW, NULL, 0);
+    /* Not needed, as we already sent two commands at minimum, which terminate data transfer. */
+    //il3820_write_cmd(IL3820_CMD_TERMINATE_FRAME_RW, NULL, 0);
 }
 
 /* Clear the graphic RAM. */
@@ -407,12 +470,12 @@ static void il3820_clear_cntlr_mem(uint8_t ram_cmd, bool update)
 
     /* Send clear_page buffer to the display */
     for(int j = 0; j < EPD_PANEL_HEIGHT; j++) {
-	il3820_set_cursor(0, j);
-	il3820_write_cmd(ram_cmd, clear_page, sizeof clear_page);
+        il3820_set_cursor(0, j);
+        il3820_write_cmd(ram_cmd, clear_page, sizeof clear_page);
     }
 
     if (update) {
-	il3820_set_window( 0, EPD_PANEL_WIDTH - 1, 0, EPD_PANEL_HEIGHT - 1);
-	il3820_update_display();
+        il3820_set_window( 0, EPD_PANEL_WIDTH - 1, 0, EPD_PANEL_HEIGHT - 1);
+        il3820_update_display();
     }
 }
